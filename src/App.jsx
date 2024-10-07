7bf3b7d2212998edf1eb6bdce3976ce126baf5cb
salgotravishav <salgotravishav2002@gmail.com>
2024-10-07 14:28:35 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+ 
+
+  return <>
+    <h1 className="text-4xl font-bold">Redux Todo App</h1>
+    <AddTodo/>
+    <Todos/>
+  </>;
+}
+
+export default App;
